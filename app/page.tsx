bea085764b49f18eca12e9dc9a8b1c2beee4497e
Matthew Manning <mattlovestech@github.com>
2025-03-02 16:42:59 -0500

    Fix: Import Footer component and render it in the MetatagsAnalyzer component

--- app/page.tsx
+++ app/page.tsx
@@ -586,6 +586,7 @@ export default function MetatagsAnalyzer() {
           </div>
         </div>
       </div>
+      
       <Footer />
 
     </div>
--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="bg-white py-4">
+        <br/>
+        <hr/>
+        <br/>
       <div className="container mx-auto text-center">
         <p className="text-gray-600">
           Created with ❤️ in <span className="font-bold">New York City</span>
